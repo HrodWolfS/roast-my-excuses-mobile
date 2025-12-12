ec7a9bb231b82add4f177c98e339dd6f65c08393
HrodWolfDev <102832449+HrodWolfS@users.noreply.github.com>
2025-12-12 13:08:10 +0100

    Test: ajout du fichier test pour le composant CircularTimer

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,6 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ['@babel/preset-env', { targets: { node: 'current' } }],
-      '@babel/preset-react'
-    ],
+    presets: ["babel-preset-expo"],
   };
 };
